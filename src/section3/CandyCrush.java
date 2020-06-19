915898de6183fd37714ac0180bd405a1ec8746eb
root <root@4c8cfa2ce08a>
2020-06-19 21:54:14 +0000

    finished section 3

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,13 +6,13 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
-		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
 		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
+//		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
 		// 3. Ask the user for the name of their best friend
-
+		JOptionPane.showMessageDialog(null, "what is the name of your best friend?");
 		// 4. Tell them their best friend is as sweet as candy
-
+		JOptionPane.showMessageDialog(null, "your best friend is sweet as candy");
 	} 
 }
 
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,15 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String liquid = JOptionPane.showInputDialog(null,"do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (liquid.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"you will rule the world");
+		}
 		// 3. Otherwise, wish them good luck washing dishes.
-
+		else {
+			JOptionPane.showMessageDialog(null,"good luck washing dishes");
+		}
 	}
 }
 
